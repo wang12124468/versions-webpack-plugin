7e7452a2ac4b2210fd1d01122652bda56c23d5b5
xiaoyong <454926339@qq.com>
2019-07-29 11:59:35 +0800

    s

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,21 @@
 var process = require('child_process');
 
-function format(date, formatStr) {
-    var formatStr = '';
-    var o = {
-        "M+": date.getMonth() + 1,                 //月份   
-        "d+": date.getDate(),                    //日   
-        "h+": date.getHours(),                   //小时   
-        "m+": date.getMinutes(),                 //分   
-        "s+": date.getSeconds(),                 //秒   
-        "q+": Math.floor((date.getMonth() + 3) / 3), //季度   
-        "S": date.getMilliseconds()             //毫秒   
-    };
-    if (/(y+)/.test(formatStr)) {
-        formatStr = formatStr.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
-    }
-    for (var k in o) {
-        if (new RegExp("(" + k + ")").test(formatStr)) {
-            formatStr = formatStr.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        }
-    }
-    return formatStr;
+function format(date, fmt) {
+    var o = {   
+        "M+" : date.getMonth()+1,                 //月份   
+        "d+" : date.getDate(),                    //日   
+        "h+" : date.getHours(),                   //小时   
+        "m+" : date.getMinutes(),                 //分   
+        "s+" : date.getSeconds(),                 //秒   
+        "q+" : Math.floor((date.getMonth()+3)/3), //季度   
+        "S"  : date.getMilliseconds()             //毫秒   
+      };   
+      if(/(y+)/.test(fmt))   
+        fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));   
+      for(var k in o)   
+        if(new RegExp("("+ k +")").test(fmt))   
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+      return fmt;   
 }
 
 function getGitVersion(n) {
@@ -30,22 +26,24 @@ function getGitVersion(n) {
             template.title = 'Git';
             if (stdout) {
                 var str = stdout.split('\n')[0] || '';
-                var commitId = str.replace('commit ');
+                var commitId = str.replace('commit ', '');
                 if (commitId) {
                     template.infos.push({ key: 'commit', value: commitId });
                 }
             }
+            console.log(template);
             resolve(template);
         });
     })
 }
 
 function getDateVersion() {
+    console.log(1);
     return format(new Date(), 'yyyy-MM-dd hh:mm:ss');
 }
 
 function addColon(str) {
-    return /:：/.test(str) ? str : (str + ': ');
+    return /[:：]/.test(str) ? str : (str + ': ');
 }
 
 function getVersionsStr(versions, callback) {
@@ -97,4 +95,6 @@ module.exports.getDateVersion = getDateVersion;
 module.exports.getTemplate = getTemplate;
 module.exports.getInfo = getInfo;
 module.exports.getVersionsStr = getVersionsStr;
+
+getGitVersion();
 
